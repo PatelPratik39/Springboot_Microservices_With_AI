767f56cba0b9b325bf76b20c1a681867897321dd
PatelPratik39 <ptl.pratik717@gmail.com>
2025-04-25 19:05:32 -0500

    Database is connected with User

---
+++ userservice/src/main/java/com/fitness/userservice/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package com.fitness.userservice.dto;
+
+import com.fitness.userservice.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private UserRole role = UserRole.USER;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
